b3ffe38e0524090066f7365e6d05fedaea64b187
deathgrasp <ariel.talmi@gmail.com>
2017-03-19 17:41:55 +0200

    half working spawner

--- Assets/Scripts/ShipSpawner.cs
+++ Assets/Scripts/ShipSpawner.cs
@@ -3,17 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Assets.Utils;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
     public class ShipSpawner : UnitySingleton<ShipSpawner>
     {
+        private static Ship _shipPrefab;
+        public static Ship ShipPrefab
+        {
+            get { return _shipPrefab ?? (_shipPrefab = Resources.Load<Ship>("Ship")); }
+        }
         public int Player1Ships;
         public int Player2Ships;
 
         public void CreateShips(Player owner)
         {
             var spawnAmount = owner.Number==1 ? Player1Ships : Player2Ships;
+            var pos = owner.Number == 1 ? new Vector3(-spawnAmount, -spawnAmount/2, 0) : new Vector3(spawnAmount, -spawnAmount/2, 0);
+            for (int i = 0; i < spawnAmount; i++)
+            {
+                //Instantiate(ShipPrefab,)
+            }
         }
 
     }
